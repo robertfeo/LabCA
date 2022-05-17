35d57ac9e201395a1c86263075e912ceb35a0ed3
selinaa64 <82643402+selinaa64@users.noreply.github.com>
2022-05-17 15:32:30 +0200

    update lab3

---
+++ Lab 3/CA3_Lab3/lab3-Funkuhr-Vorlage/Sources/dcf77.c
@@ -0,0 +1,109 @@
+/*  Radio signal clock - DCF77 Module
+
+    Computerarchitektur 3
+    (C) 2018 J. Friedrich, W. Zimmermann Hochschule Esslingen
+
+    Author:   W.Zimmermann, Jun  10, 2016
+    Modified: -
+*/
+
+/*
+; A C H T U N G:  D I E S E  S O F T W A R E  I S T  U N V O L L S T Ä N D I G
+; Dieses Modul enthält nur Funktionsrahmen, die von Ihnen ausprogrammiert werden
+; sollen.
+*/
+
+
+#include <hidef.h>                                      // Common defines
+#include <mc9s12dp256.h>                                // CPU specific defines
+#include <stdio.h>
+
+#include "dcf77.h"
+#include "led.h"
+#include "clock.h"
+#include "lcd.h"
+
+// Global variable holding the last DCF77 event
+DCF77EVENT dcf77Event = NODCF77EVENT;
+
+// Modul internal global variables
+static int  dcf77Year=2017, dcf77Month=1, dcf77Day=1, dcf77Hour=0, dcf77Minute=0;       //dcf77 Date and time as integer values
+
+// Prototypes of functions simulation DCF77 signals, when testing without
+// a DCF77 radio signal receiver
+void initializePortSim(void);                   // Use instead of initializePort() for testing
+char readPortSim(void);                         // Use instead of readPort() for testing
+
+// ****************************************************************************
+// Initalize the hardware port on which the DCF77 signal is connected as input
+// Parameter:   -
+// Returns:     -
+void initializePort(void)
+{
+// --- Add your code here ----------------------------------------------------
+// --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? ---
+    DDRH = 0; 
+
+}
+
+// ****************************************************************************
+// Read the hardware port on which the DCF77 signal is connected as input
+// Parameter:   -
+// Returns:     0 if signal is Low, >0 if signal is High
+char readPort(void)
+{
+// --- Add your code here ----------------------------------------------------
+// --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? ---
+    if((PTH&1) == 0){
+        return 0;  
+    } else{
+        return 1;  
+    }}
+
+// ****************************************************************************
+//  Initialize DCF77 module
+//  Called once before using the module
+void initDCF77(void)
+{   setClock((char) dcf77Hour, (char) dcf77Minute, 0);
+    displayDateDcf77();
+
+    initializePort();
+}
+
+// ****************************************************************************
+// Display the date derived from the DCF77 signal on the LCD display, line 1
+// Parameter:   -
+// Returns:     -
+void displayDateDcf77(void)
+{   char datum[32];
+
+    (void) sprintf(datum, "%02d.%02d.%04d", dcf77Day, dcf77Month, dcf77Year);
+    writeLine(datum, 1);
+}
+
+// ****************************************************************************
+//  Read and evaluate DCF77 signal and detect events
+//  Must be called by user every 10ms
+//  Parameter:  Current CPU time base in milliseconds
+//  Returns:    DCF77 event, i.e. second pulse, 0 or 1 data bit or minute marker
+DCF77EVENT sampleSignalDCF77(int currentTime)
+{   DCF77EVENT event = NODCF77EVENT;
+
+// --- Add your code here ----------------------------------------------------
+// --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? ---
+
+    return event;
+}
+
+// ****************************************************************************
+// Process the DCF77 events
+// Contains the DCF77 state machine
+// Parameter:   Result of sampleSignalDCF77 as parameter
+// Returns:     -
+void processEventsDCF77(DCF77EVENT event)
+{
+// --- Add your code here ----------------------------------------------------
+// --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? --- ??? ---
+
+}
+
